79a5cb570d151f188fe22dd273daaf368c6976a6
K0Mlk <k0mlk99c0de@gmail.com>
2024-12-07 12:41:33 +0500

    Dependencies have been configured

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        @vite(['resources/css/app.css', 'resources/sass/main.sass', 'resources/js/app.js'])
+        <h1>(.   )(   .)</h1>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
+
+        </div>
+    </body>
+</html>
